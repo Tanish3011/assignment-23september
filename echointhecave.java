a9c664a8d3758ede7c5dcc576a4c5ca2181d27ea
Tanish Chandra <tanishchandra30@gmail.com>
2025-09-23 21:43:15 +0530

    java

---
+++ echointhecave.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class echointhecave {
+    public static void echo(int n){
+        if(n==0) return;
+        System.out.print(n + " ");
+        echo(n-1);
+    }  
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        echo(n);
+
+    }
+    
+}
